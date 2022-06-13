b3a88737f35263e828974670718f44c3619727b0
andysierra <camilo-hormiga@hotmail.com>
2022-06-13 04:05:54 -0500

    75% learned

--- react.hooks.testing.js
+++ react.hooks.testing.js
@@ -13,6 +13,7 @@ describe('Pruebas sobre el hook useFetchGifs', ()=>{
   let result      = null;
   const category  = 'Homer Simpson';
   const limit     = 4;
+  const sleep     = ms => new Promise(resolve => setTimeout(resolve, ms));
   
   function getWrappingComponent(hook, ...params) {
     return ()=>{
@@ -43,9 +44,28 @@ describe('Pruebas sobre el hook useFetchGifs', ()=>{
 
   it('El estado del hook inicialmente debe ser {loading:true}', ()=>{
     let Component = getWrappingComponent(useFetchGifs, category, limit);
-    console.log(result);
-    act(()=>{ ReactDOM.createRoot(container).render(<Component/>); });
-    console.log(result);
-  })
+    expect(result).toBe(null);
+    act(() => ReactDOM.createRoot(container).render(<Component/>));
+    expect(result).not.toBe(null);
+    expect(result).toEqual({loading:true});
+  });
+
+
+
+
+  it(`El hook debe obtener ${limit} imagenes de la categoria ${category} en 3 segundos`, async ()=>{
+    let Component = getWrappingComponent(useFetchGifs, category, limit);
+    expect(result).toBe(null);
+    act(() => ReactDOM.createRoot(container).render(<Component/>));
+    await act(() => sleep(3000));
+
+    expect(result).not.toBe(null);
+
+    if(result != null) {
+      expect(result.gifs.length).toBe(limit);
+      expect(result.count).toBe(limit);
+      expect(result.loading).toBe(false);
+    }
+  });
 
 });
